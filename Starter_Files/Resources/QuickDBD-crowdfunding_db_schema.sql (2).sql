245ce172de796ba07eea86b3fd8a0101c0311dd4
Mun33t <muneetpruthi@gmail.com>
2025-01-30 15:54:04 -0500

    committing changes

--- Starter_Files/Resources/QuickDBD-crowdfunding_db_schema.sql (2).sql
+++ Starter_Files/Resources/QuickDBD-crowdfunding_db_schema.sql (2).sql
@@ -24,31 +24,37 @@ CREATE TABLE "Category" (
 CREATE TABLE "Subcategory" (
     "subcategory_id" VARCHAR(300)   NOT NULL,
     "subcategory" VARCHAR(255)   NOT NULL,
-    "category_id" VARCHAR(300)   NOT NULL,
     CONSTRAINT "pk_Subcategory" PRIMARY KEY (
         "subcategory_id"
      )
 );
 
 CREATE TABLE "Campaign" (
-    "id" INT   NOT NULL,
+    "cf_id" INT   NOT NULL,
     "contact_id" INT   NOT NULL,
     "company_name" VARCHAR(255)   NOT NULL,
     "description" VARCHAR(255)   NOT NULL,
     "goal" INT   NOT NULL,
     "pledged" INT   NOT NULL,
     "outcome" VARCHAR(250)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(300)   NOT NULL,
     "currency" VARCHAR(255)   NOT NULL,
     "launch_date" DATE   NOT NULL,
     "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(300)   NOT NULL,
+    "subcategory_id" VARCHAR(300)   NOT NULL,
     CONSTRAINT "pk_Campaign" PRIMARY KEY (
-        "id"
+        "cf_id"
      )
 );
 
-ALTER TABLE "Subcategory" ADD CONSTRAINT "fk_Subcategory_category_id" FOREIGN KEY("category_id")
-REFERENCES "Category" ("category_id");
-
 ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
 
